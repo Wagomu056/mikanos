a0455826a945c0091c1e0bbd8156e09363e52f3c
Takuya Azumaguchi <catchball_8@hotmail.com>
2026-04-06 00:10:55 +0900

    remove unused code

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -2,7 +2,6 @@
 #include <cstdio>
 
 #include "console.hpp"
-#include "font.hpp"
 #include "graphics.hpp"
 
 void *operator new(size_t size, void *buf) { return buf; }
@@ -34,24 +33,6 @@ extern "C" void KernelMain(const FrameBufferConfig &frame_buffer_config) {
       pixel_writer->Write(x, y, {0, 255, 0});
     }
   }
-
-  // WriteAscii(*pixel_writer, 50, 50, 'A', {0,0,0});
-  // WriteAscii(*pixel_writer, 58, 50, 'Z', {0,0,0});
-  // WriteString(*pixel_writer, 58, 50 - 18 * 2, "Hello! World!", {0, 0, 0});
-
-  /*
-  char buf[128];
-  sprintf(buf, "1 + 2 = %d", 1 + 2);
-  WriteString(*pixel_writer, 58, 50 - 18, buf, {0, 0, 0});
-
-  for (int i = 0; i < 128; ++i) {
-    char c = static_cast<char>(i);
-    int x_off = i % 32;
-    int y_off = i / 32;
-    WriteAscii(*pixel_writer, 50 + (x_off * 8), 50 + (y_off * 16), c,
-               {0, 0, 0});
-  }
-  */
 
   Console console{*pixel_writer, {0, 0, 0}, {255, 255, 255}};
 
