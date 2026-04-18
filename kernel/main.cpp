9712ae351535c6fdb2d0e3aee05179c705538491
Takuya Azumaguchi <catchball_8@hotmail.com>
2026-04-18 15:27:52 +0900

    add printk func

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -1,3 +1,4 @@
+#include <cstdarg>
 #include <cstddef>
 #include <cstdio>
 
@@ -7,6 +8,22 @@
 void *operator new(size_t size, void *buf) { return buf; }
 
 void operator delete(void *obj) noexcept {}
+
+char console_buf[sizeof(Console)];
+Console *console;
+
+int printk(const char *format, ...) {
+  va_list ap;
+  int result;
+  char s[1024];
+
+  va_start(ap, format);
+  result = vsprintf(s, format, ap);
+  va_end(ap);
+
+  console->PutString(s);
+  return result;
+}
 
 char pixel_writer_buf[sizeof(RGBResv8BitPerColorPixelWriter)];
 PixelWriter *pixel_writer;
@@ -34,12 +51,11 @@ extern "C" void KernelMain(const FrameBufferConfig &frame_buffer_config) {
     }
   }
 
-  Console console{*pixel_writer, {0, 0, 0}, {255, 255, 255}};
+  console =
+      new (console_buf) Console{*pixel_writer, {0, 0, 0}, {255, 255, 255}};
 
-  char buf[128];
   for (int i = 0; i < 27; ++i) {
-    sprintf(buf, "line %d\n", i);
-    console.PutString(buf);
+    printk("printk: %d\n", i);
   }
 
   while (1)
