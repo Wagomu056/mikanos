ebfc146e02c3067ed43b07fcd9792d1d6d52c7fc
Takuya Azumaguchi <catchball_8@hotmail.com>
2026-04-19 21:05:19 +0900

    add asm and pci

---
+++ kernel/pci.hpp
@@ -0,0 +1,7 @@
+
+#include <cstdint>
+
+/** @brief CONFIG_ADDRESS レジスタのIOポートアドレス */
+const uint16_t kConfigAddress = 0x0cf8;
+/** @brief CONFIG_DATA レジスタのIOポートアドレス */
+const uint16_t kConfigData = 0x0cfc;
