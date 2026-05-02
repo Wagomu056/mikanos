d12578fd880538b4488e903490c634126c8461bd
Takuya Azumaguchi <catchball_8@hotmail.com>
2026-05-02 12:19:55 +0900

    add pilling process of mouse

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -179,6 +179,13 @@ extern "C" void KernelMain(const FrameBufferConfig &frame_buffer_config) {
     }
   }
 
+  while (1) {
+    if (auto err = ProcessEvent(xhc)) {
+      Log(kError, "Error while ProcessEvent: %s at %s:%d\n", err.Name(),
+          err.File(), err.Line());
+    }
+  }
+
   while (1)
     __asm__("hlt");
 }
