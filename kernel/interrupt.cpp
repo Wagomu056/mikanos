2df2eda98d1dfb7d5bce861b02a775b106248906
Takuya Azumaguchi <catchball_8@hotmail.com>
2026-05-03 14:47:47 +0900

    add idt

--- kernel/interrupt.cpp
+++ kernel/interrupt.cpp
@@ -1,5 +1,9 @@
 #include "interrupt.hpp"
 
+#include <array>
+
+std::array<InterruptDescriptor, 256> idt;
+
 void NotifyEndOfInterrupt() {
   volatile auto end_of_interrupt = reinterpret_cast<uint32_t *>(0xfee000b0);
   *end_of_interrupt = 0;
