99c887eefc3af4d7e3c00ff8904b2d5ccd18d52b
Takuya Azumaguchi <catchball_8@hotmail.com>
2026-05-10 09:40:25 +0900

    add printing memory map

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -132,6 +132,28 @@ extern "C" void KernelMain(const FrameBufferConfig &frame_buffer_config,
   // LogLevel
   SetLogLevel(kInfo);
 
+  const std::array available_memory_types{
+      MemoryType::kEfiBootServicesCode,
+      MemoryType::kEfiBootServicesData,
+      MemoryType::kEfiConventionalMemory,
+  };
+
+  printk("memory_map: %p\n", &memory_map);
+  for (uintptr_t iter = reinterpret_cast<uintptr_t>(memory_map.buffer);
+       iter <
+       reinterpret_cast<uintptr_t>(memory_map.buffer) + memory_map.map_size;
+       iter += memory_map.descriptor_size) {
+    auto desc = reinterpret_cast<MemoryDescriptor *>(iter);
+    for (int i = 0; i < available_memory_types.size(); ++i) {
+      if (desc->type == available_memory_types[i]) {
+        printk("type = %u, phys = %08lx - %08lx, pages = %lu, attr = %08lx\n",
+               desc->type, desc->physical_start,
+               desc->physical_start + desc->number_of_pages * 4096 - 1,
+               desc->attribute);
+      }
+    }
+  }
+
   mouse_cursor = new (mouse_cursor_buf)
       MouseCursor{pixel_writer, kDesktopBGColor, {300, 200}};
 
--- kernel/memory_map.hpp
+++ kernel/memory_map.hpp
@@ -10,3 +10,38 @@ struct MemoryMap {
   unsigned long long descriptor_size;
   uint32_t descriptor_version;
 };
+
+struct MemoryDescriptor {
+  uint32_t type;
+  uintptr_t physical_start;
+  uintptr_t virtual_start;
+  uint64_t number_of_pages;
+  uint64_t attribute;
+};
+
+#ifdef __cplusplus
+enum class MemoryType {
+  kEfiReservedMemoryType,
+  kEfiLoaderCode,
+  kEfiLoaderData,
+  kEfiBootServicesCode,
+  kEfiBootServicesData,
+  kEfiRuntimeServicesCode,
+  kEfiRuntimeServicesData,
+  kEfiConventionalMemory,
+  kEfiUnusableMemory,
+  kEfiACPIReclaimMemory,
+  kEfiACPIMemoryNVS,
+  kEfiMemoryMappedIO,
+  kEfiMemoryMappedIOPortSpace,
+  kEfiPalCode,
+  kEfiPersistentMemory,
+  kEfiMaxMemoryType
+};
+
+inline bool operator==(uint32_t lhs, MemoryType rhs) {
+  return lhs == static_cast<uint32_t>(rhs);
+}
+
+inline bool operator==(MemoryType lhs, uint32_t rhs) { return rhs == lhs; }
+#endif
