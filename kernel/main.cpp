42561ab4d288f54ebce8ca6d26d475b19f2ef79a
Takuya Azumaguchi <catchball_8@hotmail.com>
2026-05-16 12:07:54 +0900

    add IsAvailable func

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -132,25 +132,17 @@ extern "C" void KernelMain(const FrameBufferConfig &frame_buffer_config,
   // LogLevel
   SetLogLevel(kInfo);
 
-  const std::array available_memory_types{
-      MemoryType::kEfiBootServicesCode,
-      MemoryType::kEfiBootServicesData,
-      MemoryType::kEfiConventionalMemory,
-  };
-
   printk("memory_map: %p\n", &memory_map);
   for (uintptr_t iter = reinterpret_cast<uintptr_t>(memory_map.buffer);
        iter <
        reinterpret_cast<uintptr_t>(memory_map.buffer) + memory_map.map_size;
        iter += memory_map.descriptor_size) {
     auto desc = reinterpret_cast<MemoryDescriptor *>(iter);
-    for (int i = 0; i < available_memory_types.size(); ++i) {
-      if (desc->type == available_memory_types[i]) {
-        printk("type = %u, phys = %08lx - %08lx, pages = %lu, attr = %08lx\n",
-               desc->type, desc->physical_start,
-               desc->physical_start + desc->number_of_pages * 4096 - 1,
-               desc->attribute);
-      }
+    if (IsAvailable(static_cast<MemoryType>(desc->type))) {
+      printk("type = %u, phys = %08lx - %08lx, pages = %lu, attr = %08lx\n",
+             desc->type, desc->physical_start,
+             desc->physical_start + desc->number_of_pages * kUEFIPageSize - 1,
+             desc->attribute);
     }
   }
 
